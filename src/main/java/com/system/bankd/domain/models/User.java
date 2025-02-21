2e56f7bdb0c85b6420595e092b618291f936e8c9
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-02-20 23:57:32 -0500

    feat: user model to create table

---
+++ src/main/java/com/system/bankd/domain/models/User.java
@@ -0,0 +1,37 @@
+package com.system.bankd.domain.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "user_account")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long userId;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "user_email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "user_password", nullable = false)
+    private String password;
+
+    @Column(name = "user_phone", nullable = false)
+    private String userPhone;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+}
