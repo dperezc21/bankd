44a5919a55397fc69a8cc2be6ef3d6cf0b6c103d
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-02-20 22:00:45 -0500

    feat: first commit

---
+++ src/test/java/com/system/bankd/BankdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.bankd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
