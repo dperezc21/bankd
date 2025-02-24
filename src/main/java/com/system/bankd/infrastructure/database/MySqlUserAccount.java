ba3e6ce2a468a01635c2f8223331ea5254dec9e1
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-02-24 16:11:00 -0500

    refactor: rename repository data base of user account

--- src/main/java/com/system/bankd/infrastructure/database/MySqlUserAccount.java
+++ src/main/java/com/system/bankd/infrastructure/database/MySqlUserAccount.java
@@ -5,7 +5,7 @@ import com.system.bankd.domain.models.Account;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MySqlAccountUser extends CrudRepository<Account, Long> {
+public interface MySqlUserAccount extends CrudRepository<Account, Long> {
     @Query("select c from Account c where c.accountType = ?1 and c.user.userId = ?2")
     Account getAccountByTypeAndUserId(Long userId, AccountType accountType);
 }
--- src/main/java/com/system/bankd/infrastructure/implementations/UserAccountImpl.java
+++ src/main/java/com/system/bankd/infrastructure/implementations/UserAccountImpl.java
@@ -3,27 +3,27 @@ package com.system.bankd.infrastructure.implementations;
 import com.system.bankd.domain.enums.AccountType;
 import com.system.bankd.domain.models.Account;
 import com.system.bankd.domain.repositories.UserAccountRepository;
-import com.system.bankd.infrastructure.database.MySqlAccountUser;
+import com.system.bankd.infrastructure.database.MySqlUserAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserAccountImpl implements UserAccountRepository {
 
-    @Autowired private MySqlAccountUser mySqlAccountUser;
+    @Autowired private MySqlUserAccount mySqlUserAccount;
 
     @Override
     public void saveUserAccount(Account account) {
-        this.mySqlAccountUser.save(account);
+        this.mySqlUserAccount.save(account);
     }
 
     @Override
     public Account getUserAccountById(Long accountId) {
-        return this.mySqlAccountUser.findById(accountId).orElse(null);
+        return this.mySqlUserAccount.findById(accountId).orElse(null);
     }
 
     @Override
     public Account getAccountByTypeAndUserId(Long userId, AccountType accountType) {
-        return this.mySqlAccountUser.getAccountByTypeAndUserId(userId, accountType);
+        return this.mySqlUserAccount.getAccountByTypeAndUserId(userId, accountType);
     }
 }
