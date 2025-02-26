4bdb746a360b3bc8e5f2be522082916104aa8208
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-02-25 21:15:26 -0500

    feat: get account movements

--- src/main/java/com/system/bankd/application/MovementUseCase.java
+++ src/main/java/com/system/bankd/application/MovementUseCase.java
@@ -1,15 +1,20 @@
 package com.system.bankd.application;
 
+import com.system.bankd.domain.exceptions.AccountNotFoundException;
 import com.system.bankd.domain.models.Account;
 import com.system.bankd.domain.models.Movement;
 import com.system.bankd.domain.repositories.AccountMovementRepository;
+import com.system.bankd.domain.repositories.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovementUseCase {
 
     @Autowired private AccountMovementRepository accountMovementRepository;
+    @Autowired private UserAccountRepository userAccountRepository;
 
     public void saveMovement(String name, String description, Double amount, Account account) {
         Movement movement = new Movement();
@@ -18,6 +23,12 @@ public class MovementUseCase {
         movement.setAmount(amount);
         movement.setAccount(account);
         accountMovementRepository.saveMomentOfAccount(movement);
+    }
+
+    public List<Movement> getAllMovements(Long accountId) throws AccountNotFoundException {
+        Account findAccount = this.userAccountRepository.getUserAccountById(accountId);
+        if(findAccount == null) throw new AccountNotFoundException("account not found");
+        return this.accountMovementRepository.getMomentsByAccountId(accountId);
     }
 
 }
