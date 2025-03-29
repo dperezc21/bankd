05e30fb3063b8b7fc07f47b3d21f730c8577049c
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-03-28 22:35:45 -0500

    feat: constructor to card type enum

--- src/main/java/com/system/bankd/card/domain/CardType.java
+++ src/main/java/com/system/bankd/card/domain/CardType.java
@@ -1,7 +1,16 @@
 package com.system.bankd.card.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum CardType {
-    BASIC,
-    CREDIT,
-    EXPENSIVE
+    BASIC("basic"),
+    CREDIT("credit"),
+    EXPENSIVE("expensive");
+
+    private String value;
+
+    CardType(String value) {
+        this.value = value;
+    }
 }
